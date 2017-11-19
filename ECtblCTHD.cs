26b0b75d1dad4871a6a47bb35f3be24179047d22
= <=>
2017-11-19 15:32:51 +0700

    Add new file ECtblCTHD.cs

---
+++ ECtblCTHD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLyBanHangEntity
+{
+    public class ECtblCTHD
+    {
+        private string _MaHD;
+
+        public string MaHD
+        {
+            get { return _MaHD; }
+            set { _MaHD = value; }
+        }
+        private string _MaHang;
+
+        public string MaHang
+        {
+            get { return _MaHang; }
+            set { _MaHang = value; }
+        }
+        private string _SoLuongMua;
+
+        public string SoLuongMua
+        {
+            get { return _SoLuongMua; }
+            set { _SoLuongMua = value; }
+        }
+        private string _DonGia;
+
+        public string DonGia
+        {
+            get { return _DonGia; }
+            set { _DonGia = value; }
+        }
+        private string _ThanhTien;
+
+        public string ThanhTien
+        {
+            get { return _ThanhTien; }
+            set { _ThanhTien = value; }
+        }
+    }
+}
---
+++ ECtblHang.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace QuanLyBanHangEntity
+{
+    public class ECtblHang
+    {
+        private string _MaHang;
+
+        public string MaHang
+        {
+            get { return _MaHang; }
+            set { _MaHang = value; }
+        }
+        private string _TenHang;
+
+        public string TenHang
+        {
+            get { return _TenHang; }
+            set { _TenHang = value; }
+        }
+        private string _DVT;
+
+        public string DVT
+        {
+            get { return _DVT; }
+            set { _DVT = value; }
+        }
+        private string _DonGia;
+
+        public string DonGia
+        {
+            get { return _DonGia; }
+            set { _DonGia = value; }
+        }
+        private string _NhaSX;
+
+        public string NhaSX
+        {
+            get { return _NhaSX; }
+            set { _NhaSX = value; }
+        }
+        private string _SoLuongCon;
+
+        public string SoLuongCon
+        {
+            get { return _SoLuongCon; }
+            set { _SoLuongCon = value; }
+        }
+        private string _MoTa;
+
+        public string MoTa
+        {
+            get { return _MoTa; }
+            set { _MoTa = value; }
+        }
+        private byte[] _HinhAnh;
+
+        public byte[] HinhAnh
+        {
+            get { return _HinhAnh; }
+            set { _HinhAnh = value; }
+        }
+    }
+
+}
