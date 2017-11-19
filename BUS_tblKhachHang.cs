81d640f33378d2d35ed62c7eb71186e9deb92881
= AnBui01123 <= anbui123it@gmail.com>
2017-11-19 23:39:21 +0700

    QuanLyBanHangBUS

---
+++ BUS_tblKhachHang.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using QuanLyBanHangEntity;
+using System.Data.SqlClient;
+using QuanLyBanHangDAL;
+
+namespace QuanLyBanHangBUS
+{
+    public class BUS_tblKhachHang
+    {
+        SQL_tblKhachHang sql= new SQL_tblKhachHang();
+
+        public void ThemDuLieu(ECtblKhachHang et)
+        {
+            sql.ThemDuLieu(et);
+        }
+
+        public void SuaDuLieu(ECtblKhachHang et)
+        {
+            sql.SuaDuLieu(et);
+        }
+
+        public void XoaDuLieu(ECtblKhachHang et)
+        {
+            sql.XoaDuLieu(et);
+        }
+
+        public DataTable TaoBang(string DieuKien)
+        {
+            return sql.TaoBang(DieuKien);
+        }
+
+    }
+}
