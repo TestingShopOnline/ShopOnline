2e20603cfbd71dda9249800f51f6822428fa1175
An Tran <trannguyenan281195@gmail.com>
2017-11-19 15:44:35 +0700

    Entity Layer - HoaDon & KhachHang

---
+++ ECtblHoaDon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLyBanHangEntity
+{
+    public class ECtblHoaDon
+    {
+        private string _MaHD;
+
+        public string MaHD
+        {
+            get { return _MaHD; }
+            set { _MaHD = value; }
+        }
+        private string _NgayLap;
+
+        public string NgayLap
+        {
+            get { return _NgayLap; }
+            set { _NgayLap = value; }
+        }
+        private string _MaKH;
+
+        public string MaKH
+        {
+            get { return _MaKH; }
+            set { _MaKH = value; }
+        }
+        private string _MaNV;
+
+        public string MaNV
+        {
+            get { return _MaNV; }
+            set { _MaNV = value; }
+        }
+    }
+}
---
+++ ECtblKhachHang.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QuanLyBanHangEntity
+{
+    public class ECtblKhachHang
+    {
+        private string _MaKH;
+
+        public string MaKH
+        {
+            get { return _MaKH; }
+            set { _MaKH = value; }
+        }
+        private string _TenKH;
+
+        public string TenKH
+        {
+            get { return _TenKH; }
+            set { _TenKH = value; }
+        }
+        private string _MaLoaiKH;
+
+        public string MaLoaiKH
+        {
+            get { return _MaLoaiKH; }
+            set { _MaLoaiKH = value; }
+        }
+        private string _Diachi;
+
+        public string Diachi
+        {
+            get { return _Diachi; }
+            set { _Diachi = value; }
+        }
+        private string _DienThoai;
+
+        public string DienThoai
+        {
+            get { return _DienThoai; }
+            set { _DienThoai = value; }
+        }
+        private string _TinhTrang;
+
+        public string TinhTrang
+        {
+            get { return _TinhTrang; }
+            set { _TinhTrang = value; }
+        }
+
+ 
+    }
+}
