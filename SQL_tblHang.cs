1cb6a021e3f45ade31ed063f53553177000a76b1
= AnBui01123 <= anbui123it@gmail.com>
2017-11-19 23:28:04 +0700

    QuanLyBanHangDAL

---
+++ SQL_tblHang.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using QuanLyBanHangEntity;
+
+namespace QuanLyBanHangDAL
+{
+    public class SQL_tblHang
+    {
+        KetNoiDB cn = new KetNoiDB();
+
+        public void ThemDuLieu(ECtblHang et)
+        {
+            cn.ThucThiCauLenhSQL(@"INSERT INTO tblHang(MaHang, TenHang, DVT, DonGiaHang, SoLuongCon, NhaSX, MoTa) VALUES ( N'"+ et.MaHang +"',N'" + et.TenHang + "',N'" + et.DVT +"',N'"+ et.DonGia+"',N'" + et.SoLuongCon + "',N'"+ et.NhaSX +"','" + et.MoTa +"')");
+        }
+        public void SuaDuLieu(ECtblHang et)
+        {
+            cn.ThucThiCauLenhSQL(@"UPDATE  tblHang SET TenHang ='" + et.TenHang + "', DVT ='" + et.DVT + "', DonGiaHang ='" + et.DonGia + "',NhaSX ='" + et.NhaSX + "', SoLuongCon ='" + et.SoLuongCon + "',MoTa='" + et.MoTa + "' where MaHang='" + et.MaHang + "'");
+        }
+        public void XoaDuLieu(ECtblHang et)
+        {
+            cn.ThucThiCauLenhSQL(@"DELETE from tblHang where MaHang='" + et.MaHang + "'");
+        }
+        public DataTable TaoBang(string DieuKien)
+        {
+            return cn.GetDataTable("Select * from tblHang " + DieuKien);
+
+        }
+    }
+}
---
+++ SQL_tblHoaDon.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuanLyBanHangEntity;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLyBanHangDAL
+{
+    public class SQL_tblHoaDon
+    {
+        KetNoiDB cn = new KetNoiDB();
+
+        public void ThemDuLieu(ECtblHoaDon et)
+        {
+            cn.ThucThiCauLenhSQL(@"INSERT INTO tblHoaDon (MaHD, NgayLap, MaKH, MaNV) VALUES  (N'" + et.MaHD + "',N'" + et.NgayLap + "',N'" + et.MaKH + "',N'" + et.MaNV + "')");
+        }
+        public void SuaDuLieu(ECtblHoaDon et)
+        {
+            cn.ThucThiCauLenhSQL(@"UPDATE  tblHoaDon SET NgayLap ='"+et.NgayLap +"', MaKH ='" + et.MaKH +"', MaNV ='" +et.MaNV +"' where MaHD='" +et.MaHD+"'");
+        }
+        public void XoaDuLieu(ECtblHoaDon et)
+        {
+            cn.ThucThiCauLenhSQL(@"DELETE from tblHoaDon where MaHD=N'" + et.MaHD + "'");
+        }
+        public DataTable LayDuLieu(string DieuKien)
+        {
+            return cn.GetDataTable("Select * from tblHoaDon" + DieuKien);
+
+        }
+        public DataTable LayTTKH(string DieuKien)
+        {
+            return cn.GetDataTable("select MaKH,TenKH from tbtKhachHang " + DieuKien);
+
+        }
+        public DataTable LayTTNV(string DieuKien)
+        {
+            return cn.GetDataTable("select MaNV,TenNV from tblNhanVien " + DieuKien);
+
+        }
+    }
+}
